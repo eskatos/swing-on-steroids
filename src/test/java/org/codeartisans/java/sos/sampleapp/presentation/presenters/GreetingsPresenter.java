8e488286b340e80788517328b211b3c8093f3132
Paul MERLIN <paul@nosphere.org>
2009-12-26 16:18:48 +0100

    Simple model for the SOS Sample Application

---
+++ src/test/java/org/codeartisans/java/sos/sampleapp/presentation/presenters/GreetingsPresenter.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2009 Paul Merlin <paul@nosphere.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.codeartisans.java.sos.sampleapp.presentation.presenters;
+
+import org.codeartisans.java.sos.presenters.BasePresenter;
+import org.codeartisans.java.sos.presenters.Presenter;
+import org.codeartisans.java.sos.sampleapp.domain.GreetService;
+import org.codeartisans.java.sos.sampleapp.presentation.views.GreetingsView;
+import org.codeartisans.java.sos.views.notifications.ClickHandler;
+import org.codeartisans.java.sos.views.notifications.ClickNotification;
+import org.codeartisans.java.toolbox.async.AsyncCallback;
+
+/**
+ * @author Paul Merlin <paul@nosphere.org>
+ */
+public class GreetingsPresenter
+        extends BasePresenter<GreetingsView>
+        implements Presenter
+{
+
+    private final GreetService greetService;
+
+    public GreetingsPresenter(GreetingsView view, GreetService greetService)
+    {
+        super(view);
+        this.greetService = greetService;
+    }
+
+    @Override
+    public void onBind()
+    {
+        recordViewRegistration(view.greetButton().addClickHandler(new ClickHandler()
+        {
+
+            @Override
+            public void onClick(ClickNotification notification)
+            {
+                view.busy();
+                greetService.greet(view.nameInput().getValue(), new AsyncCallback<String>()
+                {
+
+                    @Override
+                    public void onSuccess(String value)
+                    {
+                        view.messageDisplay().setValue(value);
+                        view.done();
+                    }
+
+                    @Override
+                    public void onError(String message, Throwable cause)
+                    {
+                        cause.printStackTrace();
+                        view.messageDisplay().setValue("Unable to greet, see logs for details.");
+                        view.done();
+                    }
+
+                });
+            }
+
+        }));
+    }
+
+    @Override
+    public void onUnbind()
+    {
+    }
+
+}
---
+++ src/test/java/org/codeartisans/java/sos/sampleapp/presentation/views/swing/SwingGreetingsView.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2009 Paul Merlin <paul@nosphere.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.codeartisans.java.sos.sampleapp.presentation.views.swing;
+
+import java.awt.EventQueue;
+import org.codeartisans.java.sos.sampleapp.presentation.views.GreetingsView;
+import org.codeartisans.java.sos.views.notifications.HasClickHandlers;
+import org.codeartisans.java.sos.views.swing.notifications.JButtonHasClickHandlers;
+import org.codeartisans.java.sos.views.swing.notifications.JLabelHasStringValue;
+import org.codeartisans.java.sos.views.swing.notifications.JTextComponentHasStringValue;
+import org.codeartisans.java.sos.views.values.HasValue;
+
+/**
+ * @author Paul Merlin <paul@nosphere.org>
+ */
+public class SwingGreetingsView implements GreetingsView
+{
+
+    private GreetingsFrame delegate;
+
+    public SwingGreetingsView()
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                delegate = new GreetingsFrame();
+            }
+
+        });
+
+    }
+
+    @Override
+    public HasValue<String> nameInput()
+    {
+        return new JTextComponentHasStringValue(delegate.getInput());
+    }
+
+    @Override
+    public HasClickHandlers greetButton()
+    {
+        return new JButtonHasClickHandlers(delegate.getButton());
+    }
+
+    @Override
+    public HasValue<String> messageDisplay()
+    {
+        return new JLabelHasStringValue(delegate.getMessage());
+    }
+
+    @Override
+    public void reveal()
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                delegate.setVisible(true);
+            }
+
+        });
+    }
+
+    @Override
+    public void hide()
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                delegate.setVisible(false);
+            }
+
+        });
+    }
+
+    @Override
+    public void busy()
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                delegate.getButton().setEnabled(false);
+            }
+
+        });
+    }
+
+    @Override
+    public void done()
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                delegate.getButton().setEnabled(true);
+            }
+
+        });
+    }
+
+}
