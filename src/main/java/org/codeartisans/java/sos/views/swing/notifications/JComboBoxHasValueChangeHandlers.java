5df55650a3bbf69e37b3b0ae7dec64802600c6c4
jmt <jmt@moonwalk.(none)>
2010-02-26 18:16:03 +0100

    add JComboBoxHasValue and JComboBoxHasValueHandler support

---
+++ src/main/java/org/codeartisans/java/sos/views/swing/notifications/JComboBoxHasValueChangeHandlers.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2009 Paul Merlin <paul@nosphere.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.codeartisans.java.sos.views.swing.notifications;
+
+import javax.swing.JComboBox;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.codeartisans.java.sos.threading.WorkQueue;
+import org.codeartisans.java.sos.views.values.ValueChangeHandler;
+import org.codeartisans.java.sos.views.values.HasValueChangeHandlers;
+import org.codeartisans.java.sos.views.values.ValueChangeNotification;
+import org.codeartisans.java.sos.views.notifications.HandlerRegistration;
+
+/**
+ *
+ * @param <V>
+ * @author jean-Michel Tonneau <barsifedron@gmail.com>
+ */
+public class JComboBoxHasValueChangeHandlers<V>
+        extends JComboBoxHasValue<V>
+        implements HasValueChangeHandlers<V>
+{
+
+    WorkQueue workQueue;
+
+    public JComboBoxHasValueChangeHandlers( WorkQueue workQueue, JComboBox jComboBox )
+    {
+        super( jComboBox );
+        this.workQueue = workQueue;
+
+    }
+
+    @Override
+    public HandlerRegistration addValueChangeHandler( final ValueChangeHandler<V> handler )
+    {
+
+        final ActionListener actionListener = new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed( ActionEvent e )
+            {
+                onValueChange();
+            }
+
+            private void onValueChange()
+            {
+                workQueue.enqueue( new Runnable()
+                {
+
+                    @Override
+                    public void run()
+                    {
+                        handler.onValueChange( new ValueChangeNotification<V>( ( V ) jComboBox.getSelectedItem() ) );
+                    }
+                } );
+            }
+        };
+
+
+        jComboBox.addActionListener( actionListener );
+
+        return new HandlerRegistration()
+        {
+
+            @Override
+            public void removeHandler()
+            {
+                jComboBox.removeActionListener( actionListener );
+            }
+        };
+
+    }
+}
