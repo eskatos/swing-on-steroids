3d5981e7720992bd3e740aba7ebdd755d6c22451
Paul MERLIN <p.merlin@netheos.net>
2011-01-06 11:34:50 +0100

    Added copyright notice in headers for Fabien Barbero

--- src/main/java/org/codeartisans/java/sos/messagebus/MultiThreadDeliveryMessageBus.java
+++ src/main/java/org/codeartisans/java/sos/messagebus/MultiThreadDeliveryMessageBus.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009, Paul Merlin. All Rights Reserved.
+ * Copyright (c) 2010, Fabien Barbero. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
